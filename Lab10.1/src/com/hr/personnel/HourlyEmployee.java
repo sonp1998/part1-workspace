e4a6fce7f3810823f9e6b28b6aac7309a6550af9
sonp1998 <sonp1998@gmail.com>
2022-09-15 18:36:35 -0400

    Completed labs 10.x.

--- Lab10.1/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1/src/com/hr/personnel/HourlyEmployee.java
@@ -8,9 +8,13 @@
 
 package com.hr.personnel;
 
+import gov.irs.IllegalWageException;
 import java.time.LocalDate;
 
 public class HourlyEmployee extends Employee {
+    //constants
+    public static final double FEDERAL_MINIMUM_WAGE = 15.0;
+
     // fields
     private double rate;
     private double hours;
@@ -23,7 +27,7 @@ public class HourlyEmployee extends Employee {
         super(name, hireDate);  // delegate to superclass ctor for name, hireDate
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) {
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) throws IllegalWageException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -41,12 +45,17 @@ public class HourlyEmployee extends Employee {
         System.out.println(getName() + " paid taxes of " + taxes);
     }
 
+
     // accessor methods
     public double getRate() {
         return rate;
     }
 
-    public void setRate(double rate) {
+    public void setRate(double rate) throws IllegalWageException {
+        if (rate < FEDERAL_MINIMUM_WAGE) {
+            throw new IllegalWageException(String.format("Illegal wage: %.2f. Federal minimum wage is %.2f", rate, FEDERAL_MINIMUM_WAGE));
+              //  ("Illegal wage: " + rate + " Federal min wage is " + FEDERAL_MINIMUM_WAGE);
+        }
         this.rate = rate;
     }
 
